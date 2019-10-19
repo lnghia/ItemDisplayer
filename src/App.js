16578420f4a13c714dc02ef9b9fdb613fdc7f3c2
n3w-us3r <trongnghialedinh@gmail.com>
2019-10-19 13:40:44 +0700

    bla bla

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import ItemDisplayer from './ItemDisplayer';
 import './App.css'
 
 function App(){
+    let items=[];
+
+    for(let i of Data){
+        items.push(<ItemDisplayer item={i} />);
+    }
+
     return(
         <div className="app">
-            <ItemDisplayer item={Data[0]} />
+            {/* <ItemDisplayer item={Data[0]} />
             <ItemDisplayer item={Data[1]} />
             <ItemDisplayer item={Data[2]} />
             <ItemDisplayer item={Data[3]} />
-            <ItemDisplayer item={Data[4]} />
+            <ItemDisplayer item={Data[4]} /> */}
+            {items}
         </div>
     );
 }
